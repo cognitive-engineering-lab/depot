c15d82a4afa2d8ef6657b294e2fb558c313ea8bf
Will Crichton <wcrichto@cs.stanford.edu>
2022-11-16 09:25:00 -0800

    Make sure script has a hash bang header

--- build.mjs
+++ build.mjs
@@ -21,6 +21,24 @@ esbuild.build({
   sourcemap: debug,
   define: { REPO_ROOT: JSON.stringify(repoRoot) },
   watch,
+  plugins: [
+    {
+      name: "executable",
+      setup(build) {
+        build.onEnd(async () => {
+          let p = "dist/main.mjs";
+          // This originally used the `banner` option in esbuild, but apparently
+          // the "use strict" invocation is always put before the banner when format = CJS,
+          // so we have to manually write it ourselves.
+          let f = await fs.promises.open(p, "r+");
+          await f.chmod(fs.constants.S_IRWXU);
+          let contents = await f.readFile("utf-8");
+          await f.write(`#!/usr/bin/env node\n${contents}`, 0);
+          await f.close();
+        });
+      },
+    },
+  ],
 });
 
 fs.copy("src/assets", "dist/assets", { recursive: true });
