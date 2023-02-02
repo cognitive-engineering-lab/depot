39b51a5be24d35ef2ffac0d7ce5a1944903032da
Will Crichton <wcrichto@cs.stanford.edu>
2023-02-02 14:35:34 -0800

    Remove testTimeout key

--- src/assets/jest.config.package.cjs
+++ src/assets/jest.config.package.cjs
@@ -4,6 +4,5 @@ module.exports = {
   // from the current module's dist directory
   preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
-  testTimeout: 15000,
   roots: ['<rootDir>/tests']
 };
