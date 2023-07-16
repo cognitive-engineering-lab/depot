458c5e42fa237c926e5f072f0edcd0a74a2cd592
Will Crichton <wcrichto@cs.stanford.edu>
2023-07-15 19:42:32 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+
+BASE_URL="https://github.com/cognitive-engineering-lab/depot/releases/latest/download"
+INSTALL_DIR=$HOME/.local/bin
+
+function download() {
+  pushd $(mktemp -d)
+  
+  wget "${BASE_URL}/$1.tar.gz"
+  tar -xf $1.tar.gz
+
+  mkdir -p $INSTALL_DIR
+  mv depot $INSTALL_DIR/depot
+
+  popd
+}
+
+ARCH=$(uname -m)
+
+function pick_target() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+      download "x86_64-unknown-linux-gnu"
+      return
+    fi
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ "$ARCH" == "arm64" ]]; then
+      download "aarch64-apple-darwin"
+      return
+    elif [[ "$ARCH" == "x86_64" ]]; then
+      download "x86_64-apple-darwin"
+      return
+    fi
+  fi
+
+  echo "Prebuilt binary not available, installing from source. This may take a few minutes."
+  cargo install depot-js
+}
+
+pick_target
+
+PATH=$PATH:$INSTALL_DIR
+depot setup
