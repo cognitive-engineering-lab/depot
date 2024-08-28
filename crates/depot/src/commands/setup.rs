c18c83cb5ea12e6e6e3082234da26a9831fa7123
Will Crichton <wcrichto@cs.stanford.edu>
2024-08-28 13:28:00 -0700

    Fix biome arm64 issue with more recent pnpm

--- crates/depot/src/commands/setup.rs
+++ crates/depot/src/commands/setup.rs
@@ -70,7 +70,7 @@ impl GlobalConfig {
   }
 }
 
-const PNPM_VERSION: &str = "9.1.1";
+const PNPM_VERSION: &str = "9.9.0";
 
 async fn download_file(url: &str, mut dst: impl Write) -> Result<()> {
   let res = reqwest::get(url).await?;
