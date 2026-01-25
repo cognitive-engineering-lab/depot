c62ac6be6cef980f226ef0e4eddcea978af1432d
gavinleroy <gavinleroy6@gmail.com>
2026-01-25 18:39:53 -0500

    Nix overlay and mkDepotWebsite function

--- flake.nix
+++ flake.nix
@@ -5,50 +5,42 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      depotOverlay = final: prev: {
+        depot = final.callPackage ./nix/package.nix {
+          cleanSource = prev.lib.cleanSource;
+        };
+
+        mkDepotWebsite = final.callPackage ./nix/mk-depot-website.nix {
+          depot = final.depot;
+        };
+      };
+    in
+    {
+      overlays.default = depotOverlay;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        manifest = (pkgs.lib.importTOML ./crates/depot/Cargo.toml).package;
-        pkgs = import nixpkgs { inherit system; };
-        depot-js = pkgs.rustPlatform.buildRustPackage {
-          pname = manifest.name;
-          version = manifest.version;
-          cargoLock.lockFile = ./Cargo.lock;
-          src = pkgs.lib.cleanSource ./.;    
-          nativeBuildInputs = [ pkgs.git ];
-          # NOTE: depot's tests have a lot of external dependencies: node, biome, ...
-          # We'll ignore them for now and figure it out later ;)
-          doCheck = false;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ depotOverlay ];
+        };
+      in
+      {
+        packages = {
+          default = pkgs.depot;
         };
 
-        ci-check = pkgs.writeScriptBin "ci-check" ''
-          cargo clippy -- -D warnings && 
-          cargo fmt --check &&
-          cargo test --features dev -- --test-threads=1
-        '';
-      in {
-        packages = { default = depot-js; };
-
-        devShells = with pkgs; rec {
-          ci = mkShell {
-            buildInputs = [ 
-              ci-check 
-              cargo 
-              rustc
-              clippy 
-              nodejs_22 
-              pnpm_9 
-            ];
-
-            RUST_BACKTRACE="1";
-            RUST_LIB_BACKTRACE="1";
-            TOKIO_WORKER_THREADS="1";
-          };
-
-          default = mkShell {
-            inherit ci;
-            buildInputs = ci.buildInputs ++ [ rust-analyzer ];
-          };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ pkgs.depot ];
         };
-      });
+      }
+    );
 }
